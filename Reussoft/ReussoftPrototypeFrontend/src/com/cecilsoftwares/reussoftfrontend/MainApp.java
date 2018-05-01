a88d8bff21dd75d4c7da5dee53fd9f166a24a5dd
Plamedi L. Lusembo <Plamedi L. Lusembo@plus-pc>
2018-04-30 23:26:32 -0300

    ReussoftPrototypeFrontend first comit

---
+++ Reussoft/ReussoftPrototypeFrontend/src/com/cecilsoftwares/reussoftfrontend/MainApp.java
@@ -0,0 +1,11 @@
+package com.cecilsoftwares.reussoftfrontend;
+
+/**
+ * @author Plamedi L. Lusembo
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Começo...");
+    }
+}
