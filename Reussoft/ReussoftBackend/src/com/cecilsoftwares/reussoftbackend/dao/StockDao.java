f7749996df1d3b3b73b47af6148350cd5598b473
Plamedi L. Lusembo <Plamedi L. Lusembo@plus-pc>
2018-06-07 22:17:26 -0300

    travail de stock

--- Reussoft/ReussoftBackend/src/com/cecilsoftwares/reussoftbackend/dao/StockDao.java
+++ Reussoft/ReussoftBackend/src/com/cecilsoftwares/reussoftbackend/dao/StockDao.java
@@ -2,6 +2,7 @@ package com.cecilsoftwares.reussoftbackend.dao;
 
 import com.cecilsoftwares.reussoftmiddleend.model.Collaborateur;
 import com.cecilsoftwares.reussoftmiddleend.model.Collaborateur.CollaborateurBuilder;
+import com.cecilsoftwares.reussoftmiddleend.model.Produit;
 import com.cecilsoftwares.reussoftmiddleend.model.ProfilUtilisateur;
 import com.cecilsoftwares.reussoftmiddleend.model.ProfilUtilisateur.ProfilUtilisateurBuilder;
 import com.cecilsoftwares.reussoftmiddleend.model.Shop;
@@ -179,6 +180,110 @@ public class StockDao {
             prs.close();
             conexao.close();
         }
+        return true;
+    }
+
+    private boolean actualiserStock(Produit produit, Shop shop, int quantiteMouvement) throws ClassNotFoundException, SQLException {
+        PreparedStatement prs;
+        ResultSet res;
+        int conteur = 0;
+
+        try (Connection conexao = ConnectionFactory.getInstance().habiliterConnection()) {
+            scriptSQL = new StringBuilder("SELECT count(*) FROM stockproduit WHERE idProduit=? AND idShop=?");
+            prs = ((PreparedStatement) conexao.prepareStatement(scriptSQL.toString()));
+
+            prs.setInt(1, produit.getCode());
+            prs.setInt(2, shop.getCode());
+            res = prs.executeQuery();
+
+            if (res != null) {
+                while (res.next()) {
+                    conteur = res.getInt(1);
+                }
+            }
+
+            if (conteur > 0) {
+                scriptSQL = new StringBuilder("UPDATE stockproduit SET quantiteStock = quantiteStock + ?");
+                scriptSQL.append(" WHERE idProduit=? AND idShop=?");
+
+                prs = ((PreparedStatement) conexao.prepareStatement(scriptSQL.toString()));
+
+                prs.setInt(1, quantiteMouvement);
+                prs.setInt(2, produit.getCode());
+                prs.setInt(3, shop.getCode());
+
+                prs.execute();
+                conexao.close();
+                prs.close();
+                res.close();
+                return true;
+
+            } else {
+                scriptSQL = new StringBuilder("INSERT INTO stockproduit (quantiteStock, idProduto, idShop)");
+                scriptSQL.append(" VALUES (?, ?, ?)");
+
+                prs = ((PreparedStatement) conexao.prepareStatement(scriptSQL.toString()));
+
+                prs.setInt(1, quantiteMouvement);
+                prs.setInt(2, produit.getCode());
+                prs.setInt(3, shop.getCode());
+
+                prs.execute();
+                conexao.close();
+                prs.close();
+                res.close();
+                return true;
+            }
+        }
+    }
+
+    public boolean entrerStock(Produit produit, Shop shop, int quantiteMouvement)
+            throws ClassNotFoundException, SQLException {
+        PreparedStatement prs;
+
+        try (Connection conexao = ConnectionFactory.getInstance().habiliterConnection()) {
+            scriptSQL = new StringBuilder("INSERT INTO entreestoque(");
+            scriptSQL.append("idProduto, quantiteProduit)");
+            scriptSQL.append(" VALUES (?, ?)");
+
+            prs = ((PreparedStatement) conexao.prepareStatement(scriptSQL.toString()));
+
+            prs.setInt(1, produit.getCode());
+            prs.setInt(2, quantiteMouvement);
+
+            prs.execute();
+
+            actualiserStock(produit, shop, quantiteMouvement);
+
+            prs.close();
+            conexao.close();
+        }
+
+        return true;
+    }
+
+    public boolean sortirStock(Produit produit, Shop shop, int quantiteMouvement)
+            throws ClassNotFoundException, SQLException {
+        PreparedStatement prs;
+        StringBuilder scriptSQL;
+        try (Connection conexao = ConnectionFactory.getInstance().habiliterConnection()) {
+            scriptSQL = new StringBuilder("INSERT INTO sortiestock(");
+            scriptSQL.append("idProduto, quantiteProduit)");
+            scriptSQL.append(" VALUES (?, ?)");
+
+            prs = ((PreparedStatement) conexao.prepareStatement(scriptSQL.toString()));
+
+            prs.setInt(1, produit.getCode());
+            prs.setInt(2, quantiteMouvement);
+
+            prs.execute();
+
+            actualiserStock(produit, shop, -quantiteMouvement);
+
+            prs.close();
+            conexao.close();
+        }
+
         return true;
     }
 
